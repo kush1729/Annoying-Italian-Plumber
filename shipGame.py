1e40c3eadc0d0460400401553b3b9a9d9d1c8473
Advaith <advaithmenon23@gmail.com>
2016-11-29 12:52:37 +0530

    Commit
    Shipgame has been updated

--- shipGame.py
+++ shipGame.py
@@ -5,6 +5,7 @@ pygame.init()
 black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
+yellow = (255, 255, 0)
 blue = (0, 0, 255)
 white = (255, 255, 255)
 brown = (139, 69, 19)
@@ -49,6 +50,24 @@ class Projectile:
     def collide(self, screen = False):  #to check whether collision with screen or collision with ships
         if self.centre[1] >= oceanY or self.centre[0] >= width: return True
         if screen: return
+        rect1 = pygame.Rect(self.centre[0] - self.radius, self.centre[1] - self.radius, 2*self.radius, 2*self.radius)
+        target_size = aip.image_size if self.target == 'aip' else enemy.image_size
+        target_loc = aip.loc if self.target == 'aip' else enemy.loc
+        target_rect = pygame.Rect(target_loc, target_size)
+        return rect1.colliderect(target_rect)
+            
+
+def powerBars(obj):
+    length = 100
+    if obj.ID == 'aip': location = (5, 5)
+    else: location = (width - length - 5, 5)
+    if obj.hp >= 66: colour = green
+    elif 66 > obj.hp >= 33: colour = yellow
+    elif 33 > obj.hp: colour = red
+    pygame.draw.rect(gameDisplay, black, (location[0], location[1], length, 10))
+    if obj.sunk: return
+    pygame.draw.rect(gameDisplay, colour, (location[0], location[1], obj.hp*length//100, 10))
+    
 
 class AIP_Boat:
     image = pygame.image.load('boat.png')
@@ -59,6 +78,8 @@ class AIP_Boat:
     gunLength = 75
     gunTip = [0, 0]
     ID = 'aip'
+    hp = 100
+    sunk = False
 
     def draw(self):
         #blit pic of mario, currently using ugly square (same thing)
@@ -91,8 +112,11 @@ class EnemyShip:
     gunLength = 75
     gunTip = [0, 0]
     ID = 'enemy'
+    hp = 100
+    sunk = False
 
     def draw(self):
+        if self.sunk: return
         self.gunTip[0] = int(self.gunBase[0] - self.gunLength * cos(radians(self.theta)))
         self.gunTip[1] = int(self.gunBase[1] - self.gunLength * sin(radians(self.theta)))
         pygame.draw.line(gameDisplay, black, self.gunTip, self.gunBase, self.gunWidth)
@@ -111,7 +135,7 @@ class EnemyShip:
 
 aip = AIP_Boat()
 enemy = EnemyShip()
-projectile = None
+projectile = [None, None]  #[aip's projectile, enemy projectile]
 
 def gameLoop():
     global aip, projectile
@@ -122,8 +146,8 @@ def gameLoop():
                     pygame.quit()
                     quit()
                 elif event.key == pygame.K_SPACE:
-                    if projectile == None:
-                        projectile = Projectile(aip)
+                    if projectile[0] == None:
+                        projectile[0] = Projectile(aip)
         keystate = pygame.key.get_pressed()
         aip.power += keystate[pygame.K_RIGHT] - keystate[pygame.K_LEFT]
         if aip.power >= MAXPOWER: aip.power = MAXPOWER
@@ -134,11 +158,22 @@ def gameLoop():
         
         gameDisplay.fill(white)
         
-        if projectile != None:
-            projectile.move()
-            if projectile.collide(): projectile = None
+        if projectile[0] != None:
+            projectile[0].move()
+            if projectile[0].collide(screen = False):
+                if enemy.loc[0] - projectile[0].radius <= projectile[0].centre[0] <= enemy.loc[0] + enemy.image_size[0]//3 + projectile[0].radius or \
+                   enemy.loc[0] + 2*enemy.image_size[0]//3 - projectile[0].radius <= projectile[0].centre[0] <= enemy.loc[0] + enemy.image_size[0] + projectile[0].radius:
+                    enemy.hp -= 20
+                else:
+                    enemy.hp -= 50
+                if enemy.hp <= 0: enemy.sunk = True
+                projectile[0] = None
+            elif projectile[0].collide(screen = True):
+                projectile[0] = None
     
         aip.draw()
+        powerBars(aip)
+        powerBars(enemy)
         enemy.draw()
         gameDisplay.blit(ocean, (-10, oceanY))
         pygame.display.update()
